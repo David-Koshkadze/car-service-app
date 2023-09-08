448d7b4ed420a14e59834541da09d8febc480e91
David-Koshkadze <rootUS3rr@pm.me>
2023-09-08 11:48:52 +0400

    imgs transform

--- app/page.js
+++ app/page.js
@@ -6,35 +6,38 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <h1>რატომ მანქანის სერვისი?</h1>
-      <div className="bg-green-200 w-full grid place-content-center gap-4 p-6 relative">
-        <div className="bg-white rounded-md flex items-center p-4 gap-4 shadow-md -translate-x-32 hover:scale-110 transition">
-          <Image 
-            src="/icons/car.svg" 
-            width={100}
-            height={100}
-          />
-          <span>სრული მომსახურება</span>
-        </div>
-        <div className="bg-white rounded-md flex items-center p-4 gap-4 shadow-md hover:scale-110 transition">
-          <Image 
-            src="/icons/big-truck.svg" 
-            width={100}
-            height={100}
-          />
+      <div className="bg-gray-50 w-full py-16 relative h-screen">
+        <h1 className="text-3xl text-center mb-8">რატომ მანქანის სერვისი?</h1>
 
-          <span>ხარისხი და სანდოობა</span>
+        <div className="grid place-content-center gap-4 p-6 relative">
+          <div className="bg-white border border-blue-500 rounded-md flex items-center p-4 gap-4 shadow-md md:-translate-x-40 hover:scale-110 transition">
+            <Image src="/icons/car.svg" width={100} height={100} />
+            <span>სრული მომსახურება</span>
+          </div>
+          <div className="bg-white border border-blue-500 rounded-md flex items-center p-4 gap-4 shadow-md hover:scale-110 transition">
+            <Image src="/icons/big-truck.svg" width={100} height={100} />
+            <span>ხარისხი და სანდოობა</span>
+          </div>
+          <div className="bg-white border border-blue-500 rounded-md flex items-center p-4 gap-4 shadow-md md:translate-x-40 hover:scale-110 transition">
+            <Image src="/icons/rocket.svg" width={100} height={100} />
+            <span>სისწრაფე და ეფექტურობა</span>
+          </div>
         </div>
 
-        <div className="bg-white rounded-md flex items-center p-4 gap-4 shadow-md translate-x-32 hover:scale-110 transition">
-          <Image 
-            src="/icons/rocket.svg" 
-            width={100}
-            height={100}
-          />
+        <Image 
+          src="/icons/navigator.svg"
+          width={400}
+          height={400}
+          className="absolute bottom-0 hidden md:w-[200px] lg:w-[400px] md:block md:translate-x-12 lg:translate-x-1/3"
+        />
 
-          <span>სისწრაფე და ეფექტურობა</span>
-        </div>
+        <Image 
+          src="/icons/towing.svg"
+          width={400} 
+          height={400}
+          className="absolute top-12 right-0 hidden md:w-[200px] lg:w-[300px] md:block -md:translate-x-12 lg:-translate-x-1/3"
+        />
+        
       </div>
       <Partners />
     </>
