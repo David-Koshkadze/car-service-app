fb016f9ce144775f76a922941c482ecfdeebc3cd
David-Koshkadze <rootUS3rr@pm.me>
2023-09-10 16:06:28 +0400

    carousel

--- components/about.js
+++ components/about.js
@@ -6,7 +6,7 @@ import { motion, useScroll } from "framer-motion";
 export default function About() {
   return (
     <motion.section className="max-w-[85rem] mx-auto px-4 ms:px-6 md:px-8 bg-gray-200">
-      <div className="flex gap-16 items-center">
+      <div className="flex flex-col md:flex-row gap-16 items-center">
         <div className="flex-1">
           <h1 className="text-3xl mb-4">ჩვენს შესახებ</h1>
           <p>
@@ -27,8 +27,13 @@ export default function About() {
             კარგი კომპანია ჩვენ ვართ
           </p>
         </div>
-        <div className="bg-blue-300 flex-1 flex justify-center items-center">
-          <Image width={400} height={600} src="/images/truck-girl.jpg" />
+        <div className="border flex-1 flex justify-center items-center">
+          <div
+            className="rounded-2xl w-72 h-96 md:w-[400px] md:h-[600px] bg-cover"
+            style={{
+              backgroundImage: "url(/images/truck-girl.jpg)",
+            }}
+          ></div>
         </div>
       </div>
     </motion.section>
--- components/photo-gallery.js
+++ components/photo-gallery.js
@@ -1,13 +1,56 @@
-import React from 'react'
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import { Pagination, Navigation } from "swiper/modules";
+
+const images = [
+  "/images/img1.jpg",
+  "/images/img2.jpg",
+  "/images/img3.jpg",
+  "/images/img4.jpg",
+  "/images/img5.jpg",
+  "/images/img6.jpg",
+  "/images/img7.jpg",
+  "/images/img8.jpg",
+  "/images/img9.jpg",
+  "/images/img10.jpg",
+  "/images/img11.jpg",
+  "/images/img12.jpg",
+];
 
 export default function PhotoGallery() {
   return (
-    <div className="bg-teal-100 max-w-[85rem] mx-auto px-8">
+    <div className="bg-green-100 max-w-[85rem] mx-auto px-8">
       <h1 className="text-3xl py-8">ფოტო გალერეა</h1>
 
-      <div className="">
-
-      </div>
+      <Swiper
+        slidesPerView={3}
+        centeredSlides={true}
+        spaceBetween={30}
+        pagination={{
+          type: "fraction",
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {images.map((image, idx) => (
+          <SwiperSlide>
+            <div
+              className="bg-contain bg-no-repeat h-96"
+              style={{
+                backgroundImage: `url(${image})`,
+              }}
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
-  )
+  );
 }
